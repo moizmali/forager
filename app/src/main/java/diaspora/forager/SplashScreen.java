7ef958ed103b4ef97b4f0d74109a72dd66cc6f6d
Moiz Mansoor Ali <moizmansooraliis@gmail.com>
2017-10-06 16:00:20 +0530

    Added The Splash Screen Implementation & A Few Changes To The Layout Of The Activities

--- app/src/main/java/diaspora/forager/SplashScreen.java
+++ app/src/main/java/diaspora/forager/SplashScreen.java
@@ -1,5 +1,6 @@
 package diaspora.forager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +9,8 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        Intent intent = new Intent(SplashScreen.this, MainMenu.class);
+        startActivity(intent);
+        finish();
     }
 }

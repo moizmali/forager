eefa6f63a63cb21f068b55fccd04a9f8fcffb0a3
Moiz Mansoor Ali <moizmansooraliis@gmail.com>
2017-10-06 15:33:58 +0530

    Added Empty Activities

---
+++ app/src/main/java/diaspora/forager/LoginSignUp.java
@@ -0,0 +1,13 @@
+package diaspora.forager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginSignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_sign_up);
+    }
+}
---
+++ app/src/main/java/diaspora/forager/StartGame.java
@@ -0,0 +1,13 @@
+package diaspora.forager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_game);
+    }
+}

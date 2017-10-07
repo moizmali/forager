ab2b623fafe0d9968f8f695a4991891a3411a311
Moiz Mansoor Ali <moizmansooraliis@gmail.com>
2017-10-07 14:38:45 +0530

    Updated SplashScreen Activity

--- app/src/main/java/diaspora/forager/SplashScreen.java
+++ app/src/main/java/diaspora/forager/SplashScreen.java
@@ -4,25 +4,26 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashScreen extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //TODO Check if user is already signed in (if not then send to loginSignUp Page).
         // TODO check whether mobile data/WIFI is connected.
-//        firebaseAuth = FirebaseAuth.getInstance();
-//        FirebaseUser user = firebaseAuth.getCurrentUser();
-//        if (user != null) {
-        // If the user is already signed in, then go to the main menu activity.
-        startActivity(new Intent(SplashScreen.this, MainMenu.class));
-        finish();
-//        } else {
-        // If no user is signed in, go to the login-signup activity.
-//            startActivity(new Intent(SplashScreen.this, LoginSignUp.class));
-//            finish();
-//        }
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // If the user is already signed in, then go to the main menu activity.
+            startActivity(new Intent(SplashScreen.this, MainMenu.class));
+            finish();
+        } else {
+            // If no user is signed in, go to the login-signup activity.
+            startActivity(new Intent(SplashScreen.this, LoginSignUp.class));
+            finish();
+        }
     }
 
 }
